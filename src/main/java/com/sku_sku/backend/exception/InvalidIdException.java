51bdde85f03b6dcdeb2bff3f8e76a92bac84c665
aitchhhan <aitchhhan@gmail.com>
2025-03-23 02:19:52 +0900

    [feat] CalendarEvent C U D basic logic

--- src/main/java/com/sku_sku/backend/exception/InvalidIdException.java
+++ src/main/java/com/sku_sku/backend/exception/InvalidIdException.java
@@ -2,4 +2,8 @@ package com.sku_sku.backend.exception;
 
 public class InvalidIdException extends RuntimeException {
     // 이 id에 해당하는 값 없
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
 }
