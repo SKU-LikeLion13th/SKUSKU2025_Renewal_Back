1d8bb4eec2104b4c58624515a134165608236941
juheeNoh123 <juhee10131013@gmail.com>
2025-04-03 17:29:18 +0900

    feature: reviewQuizAdd complete

--- src/main/java/com/sku_sku/backend/controller/admin/ReviewQuizAdminController.java
+++ src/main/java/com/sku_sku/backend/controller/admin/ReviewQuizAdminController.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5500", "http://localhost:5173", "https://sku-sku.com"})
 @RequestMapping("/admin")
 public class ReviewQuizAdminController {
     private final ReviewQuizService reviewQuizService;
@@ -21,7 +20,7 @@ public class ReviewQuizAdminController {
             responses = {@ApiResponse(responseCode = "201", description = "생성")})
                     //@ApiResponse(responseCode = "409", description = "그 title 이미 있")})
     @PostMapping("reviewQuiz/add")
-    public ResponseEntity<String> makeReviewQuiz(@RequestBody AddQuizDTO.AddQuizRequest req) throws IOException {
+    public ResponseEntity<String> makeReviewQuiz(@ModelAttribute AddQuizDTO.AddQuizRequest req) throws IOException {
         reviewQuizService.addQuiz(req.getTitle(),req.getTrackType(),req.getReviewQuizDTOList());
         return ResponseEntity.status(HttpStatus.CREATED).body("복습퀴즈 생성 완료");
     }
--- src/main/java/com/sku_sku/backend/security/SecurityConfig.java
+++ src/main/java/com/sku_sku/backend/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5500","https://sku-sku.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://sku-sku.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
