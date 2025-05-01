6de6df625f9ef9ca9d1373c1ef9feb08bbae77fd
aitchhhan <aitchhhan@gmail.com>
2025-05-01 20:31:23 +0900

    [fix] joinLectureFiles -> joinLectureFile

--- src/main/java/com/sku_sku/backend/repository/LectureRepository.java
+++ src/main/java/com/sku_sku/backend/repository/LectureRepository.java
@@ -12,17 +12,17 @@ import java.util.Optional;
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
     // id로 강의 안내물 반환 (강의자료를 제외한)
-    @EntityGraph(attributePaths = {"joinLectureFiles"})
+    @EntityGraph(attributePaths = {"joinLectureFile"})
     Optional<Lecture> findById(Long id);
 
     // 전체 강의 안내물 리스트 반환 (강의자료를 제외한)
-    @EntityGraph(attributePaths = {"joinLectureFiles"})
+    @EntityGraph(attributePaths = {"joinLectureFile"})
     List<Lecture> findAll();
 
     // 트랙별 강의 안내물 리스트 내림차순 반환 (강의자료를 제외한)
-    @EntityGraph(attributePaths = {"joinLectureFiles"})
+    @EntityGraph(attributePaths = {"joinLectureFile"})
     List<Lecture> findByTrackOrderByIdDesc(TrackType trackType);
 
-    @EntityGraph(attributePaths = {"joinLectureFiles"})
+    @EntityGraph(attributePaths = {"joinLectureFile"})
     Optional<List<Lecture>> findByTrackOrderByCreateDateDesc(TrackType trackType);
 }
