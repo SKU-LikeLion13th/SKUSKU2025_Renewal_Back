2e6dc451ef6c06c294bb9618bc74c447b69310a0
juheeNoh123 <juhee10131013@gmail.com>
2025-07-25 22:26:19 +0900

    reviewQuiz:fix count

--- src/main/java/com/sku_sku/backend/domain/reviewquiz/ReviewQuizResponse.java
+++ src/main/java/com/sku_sku/backend/domain/reviewquiz/ReviewQuizResponse.java
@@ -42,7 +42,7 @@ public class ReviewQuizResponse {
     private QuizType quizType; // 문제 유형 MULTIPLE_CHOICE or ESSAY_QUESTION
 
     @Setter
-    private int count = 1; // 다시 푼 횟수 // 맨 처음 응답은 다시 푼 횟수가 아니라서 기본 값 0으로
+    private int count; // 푼 횟수
 
     public ReviewQuizResponse(Lion lion, ReviewQuiz reviewQuiz, String answer, QuizType quizType) {
         this.lion = lion;
@@ -51,5 +51,6 @@ public class ReviewQuizResponse {
         this.createDate = LocalDateTime.now();
         this.updateDate = this.createDate;
         this.quizType = quizType;
+        this.count=1;
     }
 }
